76a217b093d6483edc9f7f13c4631599f52e7510
AD0791 <alexdisla1991@gmail.com>
2023-01-11 16:21:58 -0500

    debut is promising

---
+++ anotherMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace anotherMVCApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
