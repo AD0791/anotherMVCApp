630ae07b90833f62fd6bfce06274b3e69df604d9
AD0791 <alexdisla1991@gmail.com>
2023-01-12 15:28:33 -0500

    done

--- anotherMVCApp/Controllers/HelloWorldController.cs
+++ anotherMVCApp/Controllers/HelloWorldController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using anotherMVCApp.Models;
+using System.Reflection;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,8 +20,13 @@ namespace anotherMVCApp.Controllers
 
         public IActionResult Hello()
         {
-            return View();
+            DogViewModel dog = new DogViewModel() { Name = "Bebetov", Age = 5 };
+            return View(dog);
         }
     }
 }
+
+
+
+
 
